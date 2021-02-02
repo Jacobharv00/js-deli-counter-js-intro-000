3a7a79b593036f1ec66ea630a28eb5082ffd3295
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 03:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   } else
-  return(`The line is currently ${i}`)
+  return(`The line is currently ${line}`)
 }
