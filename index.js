5601baa0b40aecc405c8dc0e208bb10102e833ea
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 03:54:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,17 +16,14 @@ function nowServing(katzDeliLine) {
   }
 }
 
+var line = []
 
-function currentLine(katzDeliLine) {
-   let i = 0
-   while (i < katzDeli.length) {
-     line.push(` `+[i+1]+`. `  + katzDeli[i])
-     i++;
-   }
-   if (katzDeli.length === 0) {
-     return "The line is currently empty."
-   } else
-   return(`The line is currently:` + line)
- }
-
- 
+function currentLine(katzDeli) {
+  for (let i = 0; i < katzDeli.length; i++) {
+    line.push(`${[i]},  ${katzDeli[i]}`)
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty."
+  } else
+  return(`The line is currently + line`)
+}
