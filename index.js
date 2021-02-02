2a1165924862bdf09ff39c385b1bd13b1a04aa94
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 03:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function takeANumber(katzDeli, name) {
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 
 }
+
+function nowServing(katzDeliLine) {
+  let i = 0
+  while (i < katzDeliLine.length) {
+    i++
+  }
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!'
+  }else{
+    return (`Currently serving ${katzDeliLine.shift()}.`)
+  }
+}
