b3fbcb660fa3a1aa1430a43c97cda0190368d0da
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 04:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return (`The line is currently empty.`)
+  return (`The line is currently empty.`)
   } else {
     var array = []
     for (var i = 0; i < katzDeliLine.length; i++) {
-      array.push(` ${i+1}. ${line[i]}`);
+      array.push(` ${i+1}. ${katzDeliLine[i]}`)
     }
     return `The line is currently:` + array;
   }
