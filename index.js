4846c54fda9e7f76b8efc7867878438bee9d7fb2
Jacob Harvey <jacobharv00@gmail.com>
2021-02-02 03:55:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ var line = []
 
 function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(`${[i]},  ${katzDeli[i]}`)
+    line.push(`${[i]}, ${katzDeli[i]}`)
   }
   if (katzDeli.length === 0) {
     return "The line is currently empty."
   } else
-  return(`The line is currently + line`)
+  return(`The line is currently ${i}`)
 }
